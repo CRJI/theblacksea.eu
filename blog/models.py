b140a2f46ef54a7f2d614bba75331e0e87a81fbb
Victor Nițu <victor@thesponge.eu>
2017-10-18 13:40:55 +0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,356 @@
+from datetime import date
+from itertools import chain
+from operator import attrgetter
+
+from django.db import models
+from django.shortcuts import render
+from django.http.response import Http404
+from django.utils.encoding import python_2_unicode_compatible
+from django import forms
+
+from wagtail.wagtailcore.url_routing import RouteResult
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, FieldRowPanel, MultiFieldPanel, \
+    InlinePanel, PageChooserPanel, StreamFieldPanel, TabbedInterface, ObjectList
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
+
+from wagtail.wagtailsearch import index
+
+from wagtail.wagtailcore.blocks import TextBlock, StructBlock, StreamBlock, FieldBlock, \
+    CharBlock, RichTextBlock, RawHTMLBlock
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+
+
+class StoriesPageTag(TaggedItemBase):
+    content_object = ParentalKey('blog.StoriesPage', related_name='tagged_items')
+
+class PullQuoteBlock(StructBlock):
+    quote = TextBlock("quote title")
+    attribution = CharBlock()
+
+    class Meta:
+        icon = "openquote"
+
+class ImageFormatChoiceBlock(FieldBlock):
+    field = forms.ChoiceField(choices=(
+        ('left', 'Wrap left'), ('right', 'Wrap right'), ('mid', 'Mid width'), ('full', 'Full width'),
+    ))
+
+class HTMLAlignmentChoiceBlock(FieldBlock):
+    field = forms.ChoiceField(choices=(
+        ('normal', 'Normal'), ('full', 'Full width'),
+    ))
+
+class ImageBlock(StructBlock):
+    image = ImageChooserBlock()
+    caption = RichTextBlock()
+    alignment = ImageFormatChoiceBlock()
+
+class AlignedHTMLBlock(StructBlock):
+    html = RawHTMLBlock()
+    alignment = HTMLAlignmentChoiceBlock()
+
+    class Meta:
+        icon = "code"
+
+class StreamBlock(StreamBlock):
+    h2 = CharBlock(icon="title", classname="title")
+    h3 = CharBlock(icon="title", classname="title")
+    h4 = CharBlock(icon="title", classname="title")
+    intro = RichTextBlock(icon="pilcrow")
+    paragraph = RichTextBlock(icon="pilcrow")
+    aligned_image = ImageBlock(label="Aligned image", icon="image")
+    pullquote = PullQuoteBlock()
+    aligned_html = AlignedHTMLBlock(icon="code", label='Raw HTML')
+    document = DocumentChooserBlock(icon="doc-full-inverse")
+
+class LinkFields(models.Model):
+    link_external = models.URLField("External link", blank=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    link_document = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        elif self.link_document:
+            return self.link_document.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_external'),
+        PageChooserPanel('link_page'),
+        DocumentChooserPanel('link_document'),
+    ]
+
+    api_fields = ['link_external', 'link_page', 'link_document']
+
+    class Meta:
+        abstract = True
+
+class ContactFields(models.Model):
+    telephone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+    address_1 = models.CharField(max_length=255, blank=True)
+    address_2 = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    country = models.CharField(max_length=255, blank=True)
+    post_code = models.CharField(max_length=10, blank=True)
+
+    panels = [
+        FieldPanel('telephone'),
+        FieldPanel('email'),
+        FieldPanel('address_1'),
+        FieldPanel('address_2'),
+        FieldPanel('city'),
+        FieldPanel('country'),
+        FieldPanel('post_code'),
+    ]
+
+    api_fields = ['telephone', 'email', 'address_1', 'address_2', 'city', 'country', 'post_code']
+
+    class Meta:
+        abstract = True
+
+
+# Carousel items
+class CarouselItem(LinkFields):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    embed_url = models.URLField("Embed URL", blank=True)
+    caption = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('embed_url'),
+        FieldPanel('caption'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    api_fields = ['image', 'embed_url', 'caption'] + LinkFields.api_fields
+
+    class Meta:
+        abstract = True
+
+
+# Related links
+class RelatedLink(LinkFields):
+    title = models.CharField(max_length=255, help_text="Link title")
+
+    panels = [
+        FieldPanel('title'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    api_fields = ['title'] + LinkFields.api_fields
+
+    class Meta:
+        abstract = True
+
+
+# Advert Snippet
+class AdvertPlacement(models.Model):
+    page = ParentalKey('wagtailcore.Page', related_name='advert_placements')
+    advert = models.ForeignKey('blog.Advert', related_name='+')
+
+    api_fields = ['advert']
+
+@python_2_unicode_compatible
+class Advert(models.Model):
+    page = models.ForeignKey(
+        'wagtailcore.Page',
+        related_name='adverts',
+        null=True,
+        blank=True
+    )
+    url = models.URLField(null=True, blank=True)
+    text = models.CharField(max_length=255)
+
+    panels = [
+        PageChooserPanel('page'),
+        FieldPanel('url'),
+        FieldPanel('text'),
+    ]
+
+    api_fields = ['page', 'url', 'text']
+
+    def __str__(self):
+        return self.text
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
+    ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(BlogIndexPage, self).get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['posts'] = blogpages
+        return context
+
+class StoriesIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
+    ]
+
+    # def serve(self, request):
+    #     # Get stories
+    #     stories = self.get_children().live().order_by('-first_published_at')
+
+    #     return render(request, self.template, {
+    #         'page': self,
+    #         'stories': stories,
+    #     })
+
+    def get_context(self, request):
+
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(StoriesIndexPage, self).get_context(request)
+        stories = self.get_children().live().order_by('-first_published_at')
+        context['stories'] = stories
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        if tag:
+            stories = stories.filter(tags__name=tag)
+
+        return context
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+    image = models.ImageField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('image'),
+        FieldPanel('date'),
+        FieldPanel('intro', classname='full'),
+        FieldPanel('body', classname='full'),
+    ]
+
+class StoriesPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=255, blank=True)
+    body_richtext = RichTextField(blank=True)
+    body_blocks = StreamField(StreamBlock(), blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    authors = models.CharField(max_length=255, blank=True)
+    dossier = models.CharField(max_length=50, blank=True)
+    format = models.CharField(max_length=50, blank=True)
+    tags = ClusterTaggableManager(through=StoriesPageTag, blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    # template = models.ForeignKey('blog.BlogTemplate')
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('authors'),
+        index.SearchField('location'),
+        index.SearchField('body_richtext'),
+        index.SearchField('body_blocks'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
+        ImageChooserPanel('image'),
+        FieldPanel('date'),
+        FieldPanel('authors'),
+        FieldPanel('location'),
+    ]
+    promote_panels = Page.promote_panels + [
+        FieldPanel('tags'),
+    ]
+
+    main_content_panels = [
+        FieldPanel('body_richtext', classname='full'),
+        StreamFieldPanel('body_blocks', classname='full'),
+    ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(main_content_panels, heading='Content body'),
+        ObjectList(content_panels, heading='Content details'),
+        ObjectList(Page.promote_panels, heading='Promote'),
+        ObjectList(Page.settings_panels, heading='Settings', classname="settings"),
+    ])
+
+class AboutPage(Page):
+    intro = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
+        FieldPanel('body', classname='full'),
+    ]
+
+
+class AuthorPage(Page):
+    intro = RichTextField(blank=True)
+    image = models.ImageField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
+        FieldPanel('image'),
+    ]
+
+    def get_context(self, request):
+
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(AuthorPage, self).get_context(request)
+        blog_list = BlogPage.objects.live()
+        story_list = StoriesPage.objects.live()
+
+        context['all_posts'] = sorted(
+            chain(blog_list, story_list),
+            key=attrgetter('date'))
+
+        return context
