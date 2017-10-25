294cfdcaa75af4adad6491e379e5d10cf6cfe2e2
Victor Nițu <victor@thesponge.eu>
2017-10-25 18:02:45 +0300

    Refactor apps and some cleanup

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 from datetime import date
 from itertools import chain
 from operator import attrgetter
@@ -354,3 +355,35 @@ class AuthorPage(Page):
             key=attrgetter('date'))
 
         return context
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('image')
+    ]
+
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+
+        # Add extra variables and return the updated context
+        blog_list = BlogPage.objects.live()
+        story_list = StoriesPage.objects.live()
+        context['all_posts'] = sorted(
+            chain(blog_list, story_list),
+            key=attrgetter('date'))
+        return context
+
+class StaticPage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname='full')
+    ]
