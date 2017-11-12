26dab72f54a8b4c67c7c760cd627f487a65835e4
Victor Nițu <victor@thesponge.eu>
2017-11-13 01:42:52 +0200

    Remove Travis for now

--- scripts/travis/install_elasticsearch5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo sysctl -w vm.max_map_count=262144
-
-sudo apt-get autoremove --purge elasticsearch
-wget -P /tmp/ https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.3.deb
-sudo dpkg -i /tmp/elasticsearch-5.3.3.deb
-sudo service elasticsearch start
