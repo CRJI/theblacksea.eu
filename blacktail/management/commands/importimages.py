31950bd4c69d4712c8ebc228b27dc31fb649ce6f
Victor Nițu <victor@thesponge.eu>
2018-03-27 14:38:10 +0300

    Command to recursively import images from a given location (#55)
    Command to recursively import images from a given location
    
    Syntax:
    
    ```
    $ ./manage.py importimages [-n|--no-commit] <start_id> <collection_id> <path_to_import_from>
    ```
    
    Example:
    
    ```
    $ ./manage.py importimages 100 2 ../tbs-old/htdocs/RES/uploads/images
    ```

---
+++ blacktail/management/commands/importimages.py
@@ -0,0 +1,80 @@
+import os
+import shutil
+from pathlib import Path
+from willow.image import Image
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+cursor = connection.cursor()
+
+class WillowImage():
+
+    def __init__(self, path):
+        fp = open(path, 'rb')
+        img = Image.open(fp)
+        dimensions = img.get_size()
+
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+
+
+class Command(BaseCommand):
+    help = "Imports all images from a given path into a collection"
+
+    def add_arguments(self, parser):
+        parser.add_argument('start_id', type=int)
+        parser.add_argument('collection_id', type=int)
+        parser.add_argument('images_path', nargs='+', type=str)
+
+        parser.add_argument('-n', '--no-commit', dest='dry', action='store_true')
+        parser.set_defaults(no_commit=False)
+
+    def handle(self, *args, **options):
+        image_id = options['start_id']
+        collection_id = options['collection_id']
+        dry = options['dry']
+
+        def copy_image(src, dest):
+            destination_path = dest.replace('imported/images/', 'imported/')
+            destination_dir = os.path.dirname(destination_path)
+            if not os.path.exists(destination_dir):
+                os.makedirs(destination_dir)
+            if not dry:
+                shutil.copy(src, destination_path)
+            return destination_path
+
+        extensions = ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG', 'gif']
+
+        for directory in options['images_path']:
+            if os.path.exists('./images'):
+                os.unlink('./images')
+            os.symlink(directory, './images')
+            for extension in extensions:
+                pathlist = Path('./images').glob(f'**/*.{extension}')
+                for path in pathlist:
+                    path_str = str(path)
+                    destination_path = copy_image(
+                        path_str,
+                        os.path.join('./media/original_images/imported/', path)
+                    )
+                    img = WillowImage(destination_path)
+
+                    title = (' '.join(path_str.split('/')[1:])).replace(f'.{extension}', '')
+                    width = img.width
+                    height = img.height
+                    file_path = destination_path.replace('./media/', '')
+                    file_size = path.stat().st_size
+
+                    sql = ('INSERT INTO wagtailimages_image '
+                           '("id", "title", "file", "width", "height", "created_at", "uploaded_by_user_id", "file_size", "collection_id") '
+                           f"VALUES ('{image_id}', '{title}', '{file_path}', '{width}', '{height}', NOW(), '1', '{file_size}', '{collection_id}');"
+                    )
+                    print(sql)
+                    if not dry:
+                        cursor.execute(sql)
+                    image_id += 1
+
+                    if dry:
+                        print(title, width, height, file_path, file_size, '\n')
+
+            os.unlink('./images')
