0addc062da58a732619c3383d20d1ff58fd519e9
Victor Nițu <victor@thesponge.eu>
2018-05-10 09:58:57 +0300

    [admin] Add date and sorting on pages list

--- blacktail/urls.py
+++ blacktail/urls.py
@@ -10,8 +10,14 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from .admin import pages
+
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
+
+    url(r'^admin/pages/$', pages.index, name='wagtailadmin_explore_root'),
+    url(r'^admin/pages/(\d+)/$', pages.index, name='wagtailadmin_explore'),
+
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
