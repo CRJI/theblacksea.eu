dd27f8c4f01ca7f7fa7c21c4d55a0c72d3013cf3
Alex Morega <alex@grep.ro>
2018-10-02 16:56:39 +0300

    redirect URLs for blogs

--- scripts/get_redirect_map.py
+++ scripts/get_redirect_map.py
@@ -7,7 +7,8 @@ from pathlib import Path
 import json
 import requests
 
-CURRENT_MAP = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQAOgWzHGdtel374RTvjB8iazqSFYXjh6VhlVR8XlV5BL7tlxvzc_70FemaAPkplCgoQFqADxMkJru2/pub?gid=1480092930&single=true&output=csv'
+STORIES_MAP = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQAOgWzHGdtel374RTvjB8iazqSFYXjh6VhlVR8XlV5BL7tlxvzc_70FemaAPkplCgoQFqADxMkJru2/pub?gid=1480092930&single=true&output=csv'
+BLOGS_MAP = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQAOgWzHGdtel374RTvjB8iazqSFYXjh6VhlVR8XlV5BL7tlxvzc_70FemaAPkplCgoQFqADxMkJru2/pub?gid=50424262&single=true&output=csv'
 
 
 def load_sheet(url):
@@ -22,7 +23,12 @@ def strip(url):
 
 
 def main():
-    redirect = {strip(old): strip(new) for old, new in load_sheet(CURRENT_MAP)}
+    redirect = {
+            strip(old): strip(new)
+            for old, new in
+            load_sheet(STORIES_MAP) + load_sheet(BLOGS_MAP)
+            if new
+        }
     path = Path(__file__).parent.parent / 'redirect.json'
     with path.open('w', encoding='utf8') as f:
         print(json.dumps(redirect, indent=2, sort_keys=True), file=f)
